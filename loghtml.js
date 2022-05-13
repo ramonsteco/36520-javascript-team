c62cdd892eff1030a866080f764ca1915561a093
ramonsteco <teco@ramons.digital>
2022-05-13 16:51:27 -0300

    loghtml

---
+++ loghtml.js
@@ -0,0 +1,13 @@
+(function () {
+    var old = console.log;
+    document.body.innerHTML += '<div id="log"></div>';
+
+    var logger = document.getElementById('log');
+    console.log = function (message) {
+        if (typeof message == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+        } else {
+            logger.innerHTML += message + '<br />';
+        }
+    }
+})();
