df5f53a84e1212f45b73c30888d649c5566ccd64
ramonsteco <teco@ramons.digital>
2022-05-19 12:47:40 -0300

    bootstrap y favicons

--- proyecto/script.js
+++ proyecto/script.js
@@ -2,8 +2,11 @@ console.log(`formBusqueda `);
 const form = document.getElementById('formBusqueda');
 console.log(`formBusqueda ${formBusqueda}`);
 form.onsubmit = formBuscarSubmit;
-function formBuscarSubmit(event) {
-    console.log(`formBuscarSubmit ${event}`);
 
-    event.preventDefault();
-  }
+
+function formBuscarSubmit(event) {
+  //https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=DEMO_KEY
+  console.log(`formBuscarSubmit ${event}`);
+
+  event.preventDefault();
+}
